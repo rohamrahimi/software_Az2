93e8315120bf8cf1cf80917b6b77f3017bb43a41
Roham Rahimi <rora1378@gmail.com>
2022-07-29 16:37:36 +0430

    Initialize project structure

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from decimal import Decimal
+
+class InputGetter:
+    pass
+
+class Calculator:
+    def __init__(self, operations):
+        self.operations = operations
+
+    def calculate(self, operation, operand1, operand2):
+        pass
+
+class Operator:
+    pass
+
+class Add(Operator):
+    pass
+
+class Sub(Operator):
+    pass
+
+if __name__ == "__main__":
+    pass
